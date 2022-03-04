be67662a683108cc10ed60c26f25a34acf51cf87
Mejsun <mejsunalghoul@gmail.com>
2022-03-04 14:55:56 +0000

    adding path

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const app = express();
+const path = require('path')
+const buildPath = path.join(__dirname, '..', 'build')
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
+app.use(express.static(buildPath));
 
 // Create the transporter with the required configuration for Outlook
 const transporter = nodemailer.createTransport({
